e8f9308de58f6523bb6d40dcc8dc2e4152a4c165
VaishnaviSagar <67277305+VaishnaviSagar@users.noreply.github.com>
2024-08-27 19:07:36 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,23 +1,14 @@
 class StringCalculator:
     def add(self, numbers):
-        delimiter, numbers = self._parse_delimiter(numbers)
-        number_list = self._split_numbers(numbers, delimiter)
-        self._check_negatives(number_list)
-        return self._sum_numbers(number_list)
-
-    def _parse_delimiter(self, numbers):
         if numbers.startswith('//'):
             delimiter, numbers = numbers[2:].split('\n', 1)
-            return delimiter, numbers
-        return ',', numbers
+        else:
+            delimiter = ','
 
-    def _split_numbers(self, numbers, delimiter):
-        return numbers.split(delimiter)
+        number_list = numbers.split(delimiter)
+        negatives = [int(num) for num in number_list if int(num) < 0]
 
-    def _check_negatives(self, number_list):
-        negatives = [num for num in number_list if int(num) < 0]
         if negatives:
-            raise ValueError(f"negatives not allowed: {', '.join(negatives)}")
+            raise ValueError(f"negatives not allowed: {', '.join(map(str, negatives))}")
 
-    def _sum_numbers(self, number_list):
         return sum(int(num) for num in number_list if 0 <= int(num) <= 1000)
