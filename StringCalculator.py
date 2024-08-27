5c5ebf2d349d10369ca23311a5ab037fa1ba6704
VaishnaviSagar <67277305+VaishnaviSagar@users.noreply.github.com>
2024-08-27 19:16:01 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,14 +1,33 @@
 class StringCalculator:
     def add(self, numbers):
+        if not numbers:
+            return 0
+        delimiter, numbers = self._extract_delimiter(numbers)
+        return self._sum_valid_numbers(numbers.split(delimiter))
+
+    def _extract_delimiter(self, numbers):
         if numbers.startswith('//'):
-            delimiter, numbers = numbers[2:].split('\n', 1)
-        else:
-            delimiter = ','
+            delimiter = numbers.split('\n', 1)[0][2:]  # Extract custom delimiter
+            return delimiter, numbers.split('\n', 1)[1] if len(numbers.split('\n', 1)) > 1 else ''
+        return ',', numbers
 
-        number_list = numbers.split(delimiter)
-        negatives = [int(num) for num in number_list if int(num) < 0]
+    def _sum_valid_numbers(self, number_list):
+        total = 0
+        negatives = []
+        for num in number_list:
+            total, negatives = self._process_number(num, total, negatives)
+        self._check_negatives(negatives)
+        return total
 
+    def _process_number(self, num, total, negatives):
+        if num:
+            value = int(num)
+            if value < 0:
+                negatives.append(value)
+            elif value <= 1000:
+                total += value
+        return total, negatives
+
+    def _check_negatives(self, negatives):
         if negatives:
             raise ValueError(f"negatives not allowed: {', '.join(map(str, negatives))}")
-
-        return sum(int(num) for num in number_list if 0 <= int(num) <= 1000)
